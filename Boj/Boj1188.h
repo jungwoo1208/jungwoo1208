dcfa415c7b55a7490eb7bfbc2fbe6e40dae6933c
jungwoo <gjw1972@naver.com>
2024-06-29 21:01:53 +0900

    백준

---
+++ Boj/Boj1188.h
@@ -0,0 +1,14 @@
+//
+// Created by gjw19 on 24. 6. 29.
+//
+
+#ifndef JUNGWOO1208_BOJ1188_H
+#define JUNGWOO1208_BOJ1188_H
+
+
+class Boj1188 {
+
+};
+
+
+#endif //JUNGWOO1208_BOJ1188_H
---
+++ Boj/Boj1202.h
@@ -0,0 +1,14 @@
+//
+// Created by gjw19 on 24. 6. 29.
+//
+
+#ifndef JUNGWOO1208_BOJ1202_H
+#define JUNGWOO1208_BOJ1202_H
+
+
+class Boj1202 {
+
+};
+
+
+#endif //JUNGWOO1208_BOJ1202_H
